f790007562abc6da036d0e1e330cf2fec96e0bbd
jJEERY <13723776735@163.com>
2019-03-06 20:01:09 +0800

    配置静态资源地址，实现图片的url直接访问

--- src/main/java/com/java/fiance/FianceApplication.java
+++ src/main/java/com/java/fiance/FianceApplication.java
@@ -2,12 +2,20 @@ package com.java.fiance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
  * @author wuxincheng
  */
 @SpringBootApplication
-public class FianceApplication {
+public class FianceApplication extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+        registry.addResourceHandler("/**").addResourceLocations("file:"+"picture/");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FianceApplication.class, args);
